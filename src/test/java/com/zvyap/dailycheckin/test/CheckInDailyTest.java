37b158b5d517d479c1f306e9ee5b0b00d708fa27
nsmssk <126756471+nsmssk@users.noreply.github.com>
2023-09-19 17:11:24 +0700

    Update CheckInDailyTest.java

--- src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
+++ src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
@@ -43,7 +43,7 @@ public class CheckInDailyTest {
                         .game(GameType.HONKAI_STAR_RAIL)
                         .game(GameType.GENSHIN_IMPACT)
                         .game(GameType.HONKAI_IMPACT_3RD)
-                        .build(),
+                        .build()
 
 
         ));
