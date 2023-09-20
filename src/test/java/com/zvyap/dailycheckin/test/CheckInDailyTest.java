6c82398435cafbc38f5eddeceeb41f54fd937948
nsmssk <126756471+nsmssk@users.noreply.github.com>
2023-09-20 16:52:18 +0700

    Update CheckInDailyTest.java

--- src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
+++ src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
@@ -43,6 +43,16 @@ public class CheckInDailyTest {
                         .game(GameType.HONKAI_STAR_RAIL)
                         .game(GameType.GENSHIN_IMPACT)
                         .game(GameType.HONKAI_IMPACT_3RD)
+                        .build(),
+                CheckInAction.builder()
+                        .token(HoyoToken.of(System.getenv("USER_3_ID"), System.getenv("USER_3_TOKEN"))))
+                        .webhook(WebhookInfo.builder()
+                                 .url(Sytem.getenv("WEBHOOK_URL")
+                                 .avatar()
+                                 .name("HoyoDaily")
+                                 .build())   
+                        .game(GameType.GENSHIN_IMPACT)
+                        .game(GameType.HONKAI_STAR_RAIL)
                         .build()
 
 
